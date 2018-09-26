da5e4916c7fd23c3792e26a7822b64f7d399892f
chinuteja <chinuteja2008@msitprogram.net>
2018-09-26 22:32:46 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ practice/m3/3-sum/3-sum/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int k,n,count = 0;
+        n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<arr.length;i++ ) {
+            arr[i] = scan.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i=0;i<n-2 ;i++) {
+            int j = i+1;
+            int x = n-1;
+            while (j<x) {
+                if (arr[i]+arr[j]+arr[x] == 0) {
+                    count++;
+                }
+                else if (arr[i] + arr[j] + arr[x] <0) {
+                    j--;
+                    
+                }else {
+                    x--;
+                }
+            }
+            
+        }
+    System.out.println(count); 
+    }
+}
