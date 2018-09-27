5905ba595a2b78489adb0e142fb8c1734529681b
chinuteja <chinuteja2008@msitprogram.net>
2018-09-28 00:39:21 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- practice/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
+++ practice/m4/4.1 Balanced Paranthesis/4.1 Balanced Paranthesis/Solution.java
@@ -12,10 +12,57 @@ class LinkedList {
 		head = temp; 
 	} 
 	public char pop() {
-
+        char data = head.data;
+        head = head.nextaddress;// to move pointer to next node.
+        return  data;
 	} 
-	public char isEmpty() {
-
+	public boolean isEmpty() {
+        return  head == null;
 	}
+	public char top() {
+		return head.data;
+	}
+ 
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int k = Integer.parseInt(scan.nextLine());
+		for (int i=0;i<k ;i++) {
+			String s = scan.next();
+			if(Paranthesis(s)) {
+				System.out.println("Yes");
+			}
+			else {
+				System.out.println("No");
+			}
+		}
+	}
+	public static boolean Paranthesis(String s) {
+		LinkedList l = new LinkedList();
+		int length = s.length();
+		for (int i = 0;i<length ;i++ ) {
+			char ch = s.charAt(i);
+			if (ch == '{' || ch == '(' || ch == '[') {
+				l.push(ch);
+			} else{
+				if(l.isEmpty()){
+					return false;
+				}
+				if(ch == '}' && l.top() == '{') {
+					l.pop();
+				}
+			
+			else if (ch == ']' && l.top() == '[') {
+				l.pop();
+			}
+			else if (ch == ')' && l.top() == '(') {
+				l.pop();
+			} else {
+				return false;
+			}
 
+		}}
+		return l.isEmpty();
+	}
 }
