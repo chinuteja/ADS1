fd4938700fc38a75cb2ed825613f051fb06ccc64
chinuteja <chinuteja2008@msitprogram.net>
2018-10-01 16:25:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- practice/m8/8.1 Selection Sort/8.1 Selection Sort/Solution.java
+++ practice/m8/8.1 Selection Sort/8.1 Selection Sort/Solution.java
@@ -1,6 +1,20 @@
-import java.util.*;
+/**
+ * { importing Scanner package }
+ */
+import java.util.Scanner;
+/**
+ * { importing Array package }
+ */
 import java.util.Arrays;
+/**
+ * Class for selectionsort.
+ */
 class Selectionsort{
+	/**
+	 * { method for sorting elements}
+	 *
+	 * @param      a     { parameter_description }
+	 */
     public static void sort(Comparable[] a) {
     	for (int i=0; i<a.length;i++) {
     		int min = i;
@@ -13,16 +27,35 @@ class Selectionsort{
 
     	}
     } 
+    /**
+     * { less function }
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { boolean }
+     */
     private static boolean less(Comparable i,Comparable j) {
     	return i.compareTo(j)<0;
     }
+    /**
+     * { function for swapping elements}
+     */
     private static void exchange(Comparable a[], int i, int j) {
     	Comparable swap = a[i];
     	a[i] = a[j];
     	a[j] = swap; 
     }
 }
+/**.
+ * Class for solution.
+ */
 class Solution {
+	/**
+	 * { main method }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		//Selectionsort s = new Selectionsort();
 		Scanner scan = new Scanner(System.in);
