a346b8e59012d505b2e50d02b291a0b27a710e41
chinuteja <chinuteja2008@msitprogram.net>
2018-10-02 18:43:31 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- practice/m8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/Solution.java
+++ practice/m8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/Solution.java
@@ -9,40 +9,43 @@ import java.util.Arrays;
 /**
  * Class for solution.
  */
-public class Solution {
-	/**.
-	 * Constructs the object for Solution class.
-	 */
-	private Solution() {
+public final class Solution {
+    /**.
+     * Constructs the object for Solution class.
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * { main function }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
-		int i = 0;
-		while (scan.hasNext()) {
-			sb.append(scan.nextLine() + ":");
-		}
-		String[] str = sb.toString().split(":");
-		Team[] teams = new Team[str.length];
-		for (String line : str ) {
-			String[] tokens = line.split(",");
-			teams[i++] = new Team(tokens[0], Integer.parseInt(tokens[1]),
-			            Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
-		}
-		Selectionsort.sort(teams);
+    }
+    /**.
+     * { main function }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        int i = 0;
+        while (scan.hasNext()) {
+            sb.append(scan.nextLine() + ":");
+        }
+        String[] str = sb.toString().split(":");
+        Team[] teams = new Team[str.length];
+        for (String line : str) {
+            String[] tokens = line.split(",");
+            teams[i++] = new Team(tokens[0], Integer.
+                parseInt(tokens[1]),
+                Integer.parseInt(tokens[2]), Integer.
+                parseInt(tokens[2 + 1]));
+        }
+        Selectionsort.sort(teams);
 
-		String output = Arrays.toString(teams);
-		output = output.replace("[", "").replace("]", "").replace(", ", ",");
-		System.out.println(output);
+        String output = Arrays.toString(teams);
+        output = output.replace("[", "").replace("]", "").
+                        replace(", ", ",");
+        System.out.println(output);
 
-		// for (Team team : (teams)
-		// 	System.out.print(team.toString() +",");
+        // for (Team team : (teams)
+        //  System.out.print(team.toString() +",");
 
-	}
+    }
 }
