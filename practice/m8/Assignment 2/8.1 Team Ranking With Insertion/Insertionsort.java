d58bac6d2891168f6634b9a2f0898891f5e736f8
chinuteja <chinuteja2008@msitprogram.net>
2018-10-03 16:30:26 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- practice/m8/Assignment 2/8.1 Team Ranking With Insertion/Insertionsort.java
+++ practice/m8/Assignment 2/8.1 Team Ranking With Insertion/Insertionsort.java
@@ -4,8 +4,10 @@
 class Insertionsort {
 	/**
 	 * { sort method for Insertion sort. It sorts all the elements in asscending order }
-	 * Complexity : N**2.
-	 * @param      a     { parameter_description }
+	 * Complexity : N**2.The complexity for less method is 1
+	 * and exhcnge method is 1 and inner loop iterates for N times and
+	 * outer loop iterates for another N times so overall time complexity is N**2.
+	 * @param      a     { comparable array }
 	 */
 	public static void sort(final Comparable[] a) {
 		// 
@@ -27,7 +29,7 @@ class Insertionsort {
 	 *
 	 * @param      i     { ith index element of a }
 	 * @param      j     {jth  index element of a }
-	 *complexity is 1
+	 *complexity is 1 because the only one statement is executed.
 	 * @return     { returns true if condition statisfies}
 	 */
 	private static boolean less(final Comparable i, final Comparable j) {
@@ -35,7 +37,7 @@ class Insertionsort {
     }
     /**
      * { exchanges the elements in ith index with jth indexd}
-     *
+     *The time complexity is 1. Because the statements in this method are executed only once.
      * @param      a     { comparable array }
      * @param      i     { index of a }
      * @param      j     { index of a }
