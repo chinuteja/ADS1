825ebab698077164ca881cebbadd0b717c3e094b
chinuteja <chinuteja2008@msitprogram.net>
2018-10-05 11:57:26 +0530

    "ads1_m10_a1: 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
+++ practice/m10/10.1 LinkedList using Recursion/10.1 LinkedList using Recursion/Solution.java
@@ -1,64 +1,16 @@
+/**.
+ * { imports Scanner package }
+ */
 import java.util.Scanner;
-class Linkedlist {
-	class Node {
-		Node next;
-		int data;
-		Node(int data) {
-			this.data = data;
-			this.next = null;
-		}
-	}
-	int size = 0;
-	Node start = null;
-	public void insertAt(int index, int val) {
-		start = insertAt(index, start, new Node(val), 0);
-	}
-	public Node insertAt(int index, Node first, Node elemnent, int count) {
-		if(index < 0) {
-			System.out.println("Can't insert at this position.");
-			return first;
-		}
-		if (count == index) {
-			elemnent.next = first;
-			return elemnent;
-		}
-		first.next = insertAt(index, first.next, elemnent, count + 1);
-		size++;
-		return first;
-
-	}
-	public void reverse() throws Exception {
-		if (size == 0) {
-			throw new Exception();
-		}
-		// else if (size == 0) {
-		// 	System.out.println("No elements to reverse.");
-		// 	return ;
-		// }
-		reverse(null, start);
-	}
-	public void reverse(Node previous, Node current) {
-		if (current != null ) {
-			reverse(current, current.next);
-			current.next = previous;
-		} else {
-			start = previous;
-		}
-	}
-	public String toString() {
-		String str = "";
-		Node temp = start;
-		if (temp == null) {
-			return "Empty linked list";
-		}
-		while (temp.next != null) {
-			str = str + temp.data + ", ";
-			temp = temp.next;
-		} str = str + temp.data + "";
-		return str;
-	}
-}
+/**.
+ * Class for solution.
+ */
 class Solution {
+	/**.
+	 * { main function }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Linkedlist l = new Linkedlist();
