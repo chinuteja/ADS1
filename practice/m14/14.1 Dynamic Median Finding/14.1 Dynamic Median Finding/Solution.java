3e6d0d4728184abe387b40c2d96254dbe53796f5
chinuteja <chinuteja2008@msitprogram.net>
2018-10-09 19:56:58 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m14/14.1 Dynamic Median Finding/14.1 Dynamic Median Finding/Solution.java
+++ practice/m14/14.1 Dynamic Median Finding/14.1 Dynamic Median Finding/Solution.java
@@ -1,40 +1,51 @@
+/**.
+ * imports Scanner package
+ */
 import java.util.Scanner;
-class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		MinPQ<Float> minpq = new MinPQ<Float>(n);
-		MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
-		int[] array = new int[n];
-		float median = 0.0f;
-		for (int i = 0; i < n; i++) {
-			float k = scan.nextFloat();
-			if (k > median) {
-				minpq.insert(k);
-			} else {
-				maxpq.insert(k);
-			}
-			if (maxpq.size() - minpq.size() > 1) {
-				float x = maxpq.delMax();
-				minpq.insert(x);
-			}
-			if (minpq.size() - maxpq.size() > 1) {
-				float y = minpq.delMin();
-				maxpq.insert(y);
-			}
-			if (minpq.size() == maxpq.size()) {
-				median = (minpq.min() + maxpq.max()) / 2;
-				System.out.println(median);
-			}
-			if (maxpq.size() > minpq.size()) {
-				median = maxpq.max();
-				System.out.println(median);
-			}
-			if (minpq.size() > maxpq.size()) {
-				median = minpq.min();
-				System.out.println(median);
-			}
+/**.
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object. for Solution class
+     */
+    private Solution() {
 
-		}
-	}
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        MinPQ<Float> minpq = new MinPQ<Float>(n);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
+        float median = 0.0f;
+        for (int i = 0; i < n; i++) {
+            float k = scan.nextFloat();
+            if (k > median) {
+                minpq.insert(k);
+            } else {
+                maxpq.insert(k);
+            }
+            if (maxpq.size() - minpq.size() > 1) {
+                float x = maxpq.delMax();
+                minpq.insert(x);
+            }
+            if (minpq.size() - maxpq.size() > 1) {
+                float y = minpq.delMin();
+                maxpq.insert(y);
+            }
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max()) / 2;
+                System.out.println(median);
+            }
+            if (maxpq.size() > minpq.size()) {
+                median = maxpq.max();
+                System.out.println(median);
+            }
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
+
+        }
+    }
 }
