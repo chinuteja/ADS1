1d5fba7b397da8448c1a55addfdc89fcbac44033
chinuteja <chinuteja2008@msitprogram.net>
2018-10-09 12:17:06 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/m13/13.1 isMinHeap/13.1 isMinHeap/MinPQ.java
+++ practice/m13/13.1 isMinHeap/13.1 isMinHeap/MinPQ.java
@@ -29,6 +29,7 @@ public class MinPQ <Key extends Comparable <Key>> {
 		// }
 		for (int i = 1; i < pq.length / 2; i++) {
 			if (less(2*i, i) && less(2*i + 1, i)) {
+				//System.out.println("hello");
 				return false;
 			}
 			// if (less(i + 2, i)) {
