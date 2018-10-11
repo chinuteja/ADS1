0b76736e110942195686245966f1f4e558a0d1b2
chinuteja <chinuteja2008@msitprogram.net>
2018-10-11 20:33:06 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M16/16.1 BST/16.1 BST/BST.java
+++ practice/M16/16.1 BST/16.1 BST/BST.java
@@ -1,20 +1,64 @@
-//import java.util.NosuchElementException;
+/**.
+ * Class for bst.
+ *
+ * @param      <Key>    The key
+ * @param      <Value>  The value
+ */
 public class BST <Key extends Comparable <Key>, Value> {
+	/**
+	 * Node type 
+	 */
 	private Node root;
+	/**.
+	 * Class for node.
+	 */
 	private class Node {
+		/**.
+		 * key of Book type
+		 */
 		private Book key;
+		/**
+		 * value of string type
+		 */
 		private String value;
+		/**
+		 * left of Node type
+		 */
 		private Node left;
+		/**
+		 * right of Node type
+		 */
 		private Node right;
+		/**
+		 * Constructs the object for Node class
+		 *
+		 * @param      key    The key
+		 * @param      value  The value
+		 */
 		public  Node(Book key, String value) {
 			this.key = key;
 			this.value = value;
 
 		}
 	}
+	/**
+	 * puts the value and key into BST
+	 *
+	 * @param      key    The key
+	 * @param      value  The value
+	 */
 	public void put(Book key, String value) {
 		root = put(root, key, value);
 	}
+	/**
+	 * over rides the put function
+	 *
+	 * @param      x      { Node }
+	 * @param      key    The key
+	 * @param      value  The value
+	 * Time complexity is N as it is a recursive function
+	 * @return     { returns the value if the given conditions are not satisfied else return the node }
+	 */
 	public Node put(Node x, Book key, String value ) {
 		if (x == null) return new Node(key, value);
 		int cmp = key.compareTo(x.key);
@@ -28,6 +72,13 @@ public class BST <Key extends Comparable <Key>, Value> {
 
 		return x;
 	}
+	/**
+	 * get the value of the given 
+	 *
+	 * @param      key   The key
+	 * Time complexity is N as the while iterates till it reaches null
+	 * @return     { returns the value of String type }
+	 */
 	public String get(Book key) {
 		Node x = root;
 		while (x != null) {
--- practice/M16/16.1 BST/16.1 BST/Solution.java
+++ practice/M16/16.1 BST/16.1 BST/Solution.java
@@ -1,23 +1,44 @@
+/**.
+ * imports the Scanner package
+ */
 import java.util.Scanner;
-class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
+/**.
+ * class for Solution
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.for solution class
+     * @author Teja
+     */
+    private Solution() {
+
+    }
+    /**
+     * main
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         BST bst = new BST();
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(",");
-			switch (tokens[0]) {
-				case "put":
-				   Book book = new Book(tokens[1] , tokens[2] ,Double.parseDouble(tokens[3]));
-				   bst.put(book,tokens[4]);
-				   break;
-					
-				case "get":
-				   book = new Book(tokens[1], tokens[2], Double.parseDouble(tokens[3]));
-				   System.out.println(bst.get(book));
-				   break;
-			}
-			
-		}
-		
-	}
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            switch (tokens[0]) {
+                case "put":
+                   Book book = new Book(tokens[1] , tokens[2] ,
+                    Double.parseDouble(tokens[3]));
+                   bst.put(book,tokens[4]);
+                   break;
+                    
+                case "get":
+                   book = new Book(tokens[1], tokens[2], 
+                    Double.parseDouble(tokens[3]));
+                   System.out.println(bst.get(book));
+                   break;
+            }
+            
+        }
+        
+    }
 }
+
