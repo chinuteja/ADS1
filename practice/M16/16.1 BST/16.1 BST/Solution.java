d6c5383f9fcd618d4455f2d3ea24a1d26bff1954
chinuteja <chinuteja2008@msitprogram.net>
2018-10-11 20:37:28 +0530

    "ads1_m16_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/M16/16.1 BST/16.1 BST/Solution.java
+++ practice/M16/16.1 BST/16.1 BST/Solution.java
@@ -24,22 +24,21 @@ public final class Solution {
         while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(",");
             switch (tokens[0]) {
-                case "put":
-                   Book book = new Book(tokens[1] , tokens[2]
-                    ,Double.parseDouble(tokens[3]));
-                   bst.put(book,tokens[4]);
-                   break;
-                case "get":
-                   book = new Book(tokens[1], tokens[2], 
-                    Double.parseDouble(tokens[3]));
-                   System.out.println(bst.get(book));
-                   break;
-                default:
-                    break;
+            case "put":
+                Book book = new Book(tokens[1], tokens[1 + 1],
+                                     Double.parseDouble(tokens[2 + 1]));
+                bst.put(book, tokens[2 + 2]);
+                break;
+            case "get":
+                book = new Book(tokens[1], tokens[1 + 1],
+                                Double.parseDouble(tokens[2 + 1]));
+                System.out.println(bst.get(book));
+                break;
+            default:
+                break;
             }
-            
         }
-        
+
     }
 }
 
