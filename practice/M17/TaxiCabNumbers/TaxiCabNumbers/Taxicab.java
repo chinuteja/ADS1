d822fb4d686c216b75ad594e42b747ab097cc935
chinuteja <chinuteja2008@msitprogram.net>
2018-10-12 18:50:21 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ practice/M17/TaxiCabNumbers/TaxiCabNumbers/Taxicab.java
@@ -0,0 +1,82 @@
+/**.
+ * Class for taxicab.
+ */
+public class Taxicab implements Comparable<Taxicab> {
+    int i;
+    int j;
+    long sum;
+    /**
+     * Constructs the object for taxi cab
+     *
+     * @param      i     { integer parameter }
+     * @param      j     { integer parameter }
+     */
+    public Taxicab(int i, int j) {
+        this.sum = (long)(i*i*i) + (j*j*j);
+        this.i = i;
+        this.j = j;
+    }
+    /**
+     * compares two objects
+     * Time complexity is 1 because each statement is executed only once.
+     * @param      that  The that
+     *
+     * @return     { returns 1 if this.sum > that.sum and this.i > that.i else returns -1}
+     */
+    public int compareTo(Taxicab that) {
+        if (this.sum > that.sum) return +1;
+        if (this.sum < that.sum) return -1;
+        if (this.i > that.i) return +1;
+        if (this.i < that.i) return -1;
+        return 0;
+    }
+    /**
+     * Returns a string representation of the object.
+     * TIme complexity is 1 as one statement is executed only once
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return sum + " = " + i + "^3 + " + j + "^3";
+    }
+
+    /**
+     * finds the taxi cab number
+     * Time complexity is N as for loop is executed for N times
+     * @param      N     { number of occurance of the taxi cab number}
+     * @param      M     { number of pairs that we needed}
+     *
+     * @return     { returns the taxicab number }
+     */
+    public static long findTaxicab(int N, int M) {
+        MinPQ<Taxicab> minPQ = new MinPQ<Taxicab>();
+        int n = 10000;
+        int count = 0;
+        long res = 0;
+        for (int i = 1; i <= n; i++) {
+            minPQ.insert(new Taxicab(i, i));
+        }
+
+        Taxicab prev = new Taxicab(0, 0);
+
+        int run = 1;
+        while (!minPQ.isEmpty()) {
+            Taxicab curr = minPQ.delMin();
+            if (prev.sum == curr.sum) {
+                run = run + 1;
+                if (run == M) {
+                    count++;
+                    if (count == N) {
+                        res = curr.sum;
+                        break;
+                    }
+                }
+            } else {
+                run = 1;
+            }
+            if (curr.j < n)
+                minPQ.insert(new Taxicab(curr.i, curr.j+1));
+            prev = curr;
+        }
+        return res;
+    }
+}
