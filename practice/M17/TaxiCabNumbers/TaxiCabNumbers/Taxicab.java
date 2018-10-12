d1e17d4c7fdee20463fccc0b1e466b65ce6bad2a
chinuteja <chinuteja2008@msitprogram.net>
2018-10-12 19:03:01 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M17/TaxiCabNumbers/TaxiCabNumbers/Taxicab.java
+++ practice/M17/TaxiCabNumbers/TaxiCabNumbers/Taxicab.java
@@ -33,11 +33,19 @@ public class Taxicab implements Comparable<Taxicab> {
      * @return     { returns 1 if this.sum > that.sum
      * and this.i > that.i else returns -1}
      */
-    public int compareTo(Taxicab that) {
-        if (this.sum > that.sum) {return +1;}
-        if (this.sum < that.sum) {return -1;}
-        if (this.i > that.i) {return +1;}
-        if (this.i < that.i) {return -1;}
+    public int compareTo(final Taxicab that) {
+        if (this.sum > that.sum) {
+            return +1;
+        }
+        if (this.sum < that.sum) {
+            return -1;
+        }
+        if (this.i > that.i) {
+            return +1;
+        }
+        if (this.i < that.i) {
+            return -1;
+        }
         return 0;
     }
     /**
@@ -57,7 +65,7 @@ public class Taxicab implements Comparable<Taxicab> {
      *
      * @return     { returns the taxicab number }
      */
-    public static long findTaxicab(int N, int M) {
+    public static long findTaxicab(final int N, final int M) {
         MinPQ<Taxicab> minPQ = new MinPQ<Taxicab>();
         int n = 600;
         int count = 0;
