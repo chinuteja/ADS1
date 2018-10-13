edb96823b33b94d86e19b326c0390bcdb0248171
chinuteja <chinuteja2008@msitprogram.net>
2018-10-13 11:27:24 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ practice/M18/Week-3_Assignment-1/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,41 @@
+/**.
+ * { imports the Scanner packagae }
+ */
+import java.util.Scanner;
+/**.
+ * { This is the class used to find the
+ *  top five and bottom five stock exchanges of given N companies }
+ */
+public final class  Solution {
+	/**.
+	 * Default constructor
+	 * @author Teja
+	 */
+	private Solution() {
+
+	}
+	/**
+	 * to demonstrate the solution
+	 *
+	 * @param      args  command line arguments
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		System.out.println("number .." +n);
+		//BinarySearchST<String, Double>  binarysearchst =
+		   // new BinarySearchST<String, Double>(n);
+		MinPQ<Double> minpq = new MinPQ<Double>(n);
+		MaxPQ<Double> maxpq = new MaxPQ<Double>(n);
+		while (scan.hasNext()) {
+			String[] tokens = scan.nextLine().split(",");
+			Stock stock = new Stock(tokens[0], Double.parseDouble(tokens[1]));
+			System.out.println("hello");
+			minpq.insert(Double.parseDouble(tokens[1]));
+			maxpq.insert(Double.parseDouble(tokens[1]));
+			//binarysearchst.put(stock, maxpq.delMax());
+			//binarysearchst.put(stock, minpq.delMin());
+			//System.out.println(binarysearchst.toString());
+		}
+	}
+}
