48637f2448a517a0ad9f5472f8b03615a43b0f86
chinuteja <chinuteja2008@msitprogram.net>
2018-10-15 18:43:28 +0530

    "ads1_m7_a1: 1/2 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/M7/7.1 Steque/Solution.java
+++ practice/M7/7.1 Steque/Solution.java
@@ -31,20 +31,20 @@ public final class Solution {
                     ss.pop();
                     System.out.println(ss);
                     //ss.toString();
-                    System.out.println();
+                    //System.out.println();
                     break;
                 case "enqueue":
                     ss.enqueue(tokens[1]);
                     System.out.println(ss);
                     //ss.toString();
-                    System.out.println();
+                    //System.out.println();
                     break;
                 // //System.out.println();
                 case "push" :
                     ss.push(tokens[1]);
                     System.out.println(ss);
                     //ss.toString();
-                    System.out.println();
+                    //System.out.println();
                     break;
                 default:
                     break;
