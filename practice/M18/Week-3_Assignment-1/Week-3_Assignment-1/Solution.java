acb85f3101996a29592e166a3718770473625019
chinuteja <chinuteja2008@msitprogram.net>
2018-10-24 17:56:29 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/M18/Week-3_Assignment-1/Week-3_Assignment-1/Solution.java
+++ practice/M18/Week-3_Assignment-1/Week-3_Assignment-1/Solution.java
@@ -21,28 +21,22 @@ public final class Solution {
 	public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 6; i++) {
             MaxPQ<Stock> best = new MaxPQ<Stock>(n);
             MinPQ<Stock> least = new MinPQ<Stock>(n);
-            for(int j = 0; j < 6; j++) {
+            for(int j = 0; j < n; j++) {
             String[] tokens = scan.nextLine().split(",");
             Stock stock = new Stock(tokens[0], Double.parseDouble(tokens[1]));
             best.insert(stock);
             least.insert(stock);
             }
-            for (int k = 0; k < 6; k++) {
-            System.out.println(best.delMax());
+            for (int k = 0; k < 5; k++) {
+            	System.out.println(best.delMax());
+            System.out.println();}
             //binarysearchst.put(stock1, maxpq.delMax());
+	        for (int l = 0; l < 5; l++) {
+    	        System.out.println(least.delMin());
+        	}
         }
-        System.out.println();
-        for (int l = 0; l < 6; l++) {
-            //System.out.println("min val" + minpq.delMin());
-            //binarysearchst.put(stock1,minpq.delMin());
-            System.out.println(least.delMin());
-        }
-            
-
-        }
-
 	}
 }
