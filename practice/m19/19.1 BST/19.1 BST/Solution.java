a30778d53984d04ff1316236c25ee5e52d119b1d
chinuteja <chinuteja2008@msitprogram.net>
2018-10-24 21:53:14 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m19/19.1 BST/19.1 BST/Solution.java
+++ practice/m19/19.1 BST/19.1 BST/Solution.java
@@ -21,57 +21,47 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         BinarySearchT<Book, Integer> binarysearcht = new BinarySearchT<>();
-        final int ZERO = 0;
-        final int ONE = 1;
-        final int TWO = 2;
-        final int THREE = 3;
-        final int FOUR = 4;
         while (scan.hasNext()) {
             String[] tokens = scan.nextLine().split(",");
             switch (tokens[0]) {
             case "put":
-                Book book = new Book(tokens[1], tokens[2],
-                                     Double.parseDouble(tokens[3]));
-                binarysearcht.put(book, Integer.parseInt(tokens[4]));
+                Book book = new Book(tokens[1], tokens[1+1],
+                                     Double.parseDouble(tokens[2+1]));
+                binarysearcht.put(book, Integer.parseInt(tokens[2+2]));
                 break;
             case "get":
-                Book book1 = new Book(tokens[1], tokens[2],
-                                      Double.parseDouble(tokens[3]));
+                Book book1 = new Book(tokens[1], tokens[1+1],
+                                        Double.parseDouble(tokens[2+1]));
                 System.out.println(binarysearcht.get(book1));
                 break;
             case "min":
-                //Book book2 = new Book(tokens[1],tokens[2],Double.parseDouble(tokens[3]));
+                //Book book1+1 = new Book(tokens[1],tokens[1+1],Double.parseDouble(tokens[2+1]));
                 System.out.println(binarysearcht.min());
                 break;
             case "max":
-                //Book book3 = new Book(tokens[1],tokens[2],Double.parseDouble(tokens[3]));
+                //Book book2+1 = new Book(tokens[1],tokens[1+1],Double.parseDouble(tokens[2+1]));
                 System.out.println(binarysearcht.max());
                 break;
             case "floor":
-                Book book4 = new Book(tokens[1], tokens[2],
-                                      Double.parseDouble(tokens[3]));
-                System.out.println(binarysearcht.floor(book4));
+                Book book2 = new Book(tokens[1], tokens[1+1],
+                                         Double.parseDouble(tokens[2+1]));
+                System.out.println(binarysearcht.floor(book2));
                 break;
             case "ceiling":
-                Book book5 = new Book(tokens[1], tokens[2],
-                                      Double.parseDouble(tokens[3]));
+                Book book5 = new Book(tokens[1], tokens[1+1],
+                                      Double.parseDouble(tokens[2+1]));
                 System.out.println(binarysearcht.ceiling(book5));
                 break;
             case "select" :
-                //Book book6 = new Book(tokens[1],tokens[2],
-            //Double.parseDouble(tokens[3])); 
-            //not need to create object once check select function
+                //Book book6 = new Book(tokens[1],tokens[1+1],
+                //Double.parseDouble(tokens[2+1]));
+                //not need to create object once check select function
                 System.out.println(
                     binarysearcht.select(Integer.parseInt(tokens[1])));
                 break;
             default:
                 break;
-
             }
-
         }
-
     }
-
 }
-    
