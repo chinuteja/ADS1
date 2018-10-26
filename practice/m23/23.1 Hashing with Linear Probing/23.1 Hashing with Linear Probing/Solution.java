dfbc154ee21d8ddb4928399cf7caabc9ca1f74a9
chinuteja <chinuteja2008@msitprogram.net>
2018-10-26 20:34:25 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m23/23.1 Hashing with Linear Probing/23.1 Hashing with Linear Probing/Solution.java
+++ practice/m23/23.1 Hashing with Linear Probing/23.1 Hashing with Linear Probing/Solution.java
@@ -1,31 +1,47 @@
+/**.
+ * imports Scanner package
+ */
 import java.util.Scanner;
+/**.
+ * class for Solution
+ */
 public final class Solution {
-	private Solution() {
+    /**.
+     * Constructs the object for Solution class
+     * @author Teja
+     */
+    private Solution() {
 
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int operations = scan.nextInt();
-		//System.out.println(operations);
-		scan.nextLine();
-		LinearProbingHashST<String, Integer> hashst = new LinearProbingHashST<String, Integer>();
-		for (int i = 0; i < operations; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			switch (tokens[0]) {
-			case "put":
-				hashst.put(tokens[1], Integer.parseInt(tokens[2]));
-				break;
-			case "get":
-			    System.out.println(hashst.get(tokens[1]));
-			    break;
-			case "delete":
-			    hashst.delete(tokens[1]);
-			    break;
-			case "display":
+    }
+    /**
+     * demonstraters the operations of Linear probing hash Symbol table
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int operations = scan.nextInt();
+        //System.out.println(operations);
+        scan.nextLine();
+        LinearProbingHashST<String, Integer> hashst = new
+                       LinearProbingHashST<String, Integer>();
+        for (int i = 0; i < operations; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "put":
+                hashst.put(tokens[1], Integer.parseInt(tokens[2]));
+                break;
+            case "get":
+                System.out.println(hashst.get(tokens[1]));
+                break;
+            case "delete":
+                hashst.delete(tokens[1]);
+                break;
+            case "display":
                 hashst.display();
                 break;
 
-			}
-		}
-	}
+            }
+        }
+    }
 }
