a096adb19cfab4c6a00690fa95db8ac5c3952875
chinuteja <chinuteja2008@msitprogram.net>
2018-10-27 12:55:58 +0530

    "m24_a2_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- practice/m24/2/Solution.java
+++ practice/m24/2/Solution.java
@@ -6,35 +6,35 @@ import java.util.Scanner;
  * Solution class
  */
 public final class Solution {
-	/**.
-	 * Constructs the object for solution class
-	 * @author Teja
-	 */
-	private Solution() {
+    /**.
+     * Constructs the object for solution class
+     * @author Teja
+     */
+    private Solution() {
 
-	}
-	/**.
-	 * main function which demostrates the operations
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		BinarySearchST<Integer, String> binarysearchst =
-		    new BinarySearchST<>();
-		int noofstudents = scan.nextInt();
-		scan.nextLine();
-		for (int i = 0; i < noofstudents; i++) {
-			String[] input = scan.nextLine().split(",");
-			String str = input[1] + "," + input[2];
-			binarysearchst.put(Integer.parseInt(input[0]), str);
-		}
-		int quires = scan.nextInt();
-		scan.nextLine();
-		for (int i = 0; i < quires; i++) {
-			String[] input = scan.nextLine().split(" ");
-			// binarysearchst.BE(Double.parseDouble(input[1]),
-		 //                  Double.parseDouble(input[2]));
-		}
-	}
+    }
+    /**.
+     * main function which demostrates the operations
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinarySearchST<Integer, String> binarysearchst =
+            new BinarySearchST<>();
+        int noofstudents = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < noofstudents; i++) {
+            String[] input = scan.nextLine().split(",");
+            String str = input[1] + "," + input[2];
+            binarysearchst.put(Integer.parseInt(input[0]), str);
+        }
+        int quires = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < quires; i++) {
+            String[] input = scan.nextLine().split(" ");
+            // binarysearchst.BE(Double.parseDouble(input[1]),
+         //                  Double.parseDouble(input[2]));
+        }
+    }
 }
