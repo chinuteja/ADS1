480a20185d04f9a421b05835e83ac6722bc3dcbb
chinuteja <chinuteja2008@msitprogram.net>
2018-10-28 18:01:02 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/m22/22.1 Ransom Note/22.1 Ransom Note/SeparateChainingHashST.java
+++ practice/m22/22.1 Ransom Note/22.1 Ransom Note/SeparateChainingHashST.java
@@ -1,10 +1,21 @@
 
 public class SeparateChainingHashST<Key, Value> {
+    /**
+     * intial capacity of symnbol tabe
+     */
     private static final int INIT_CAPACITY = 4;
-
-    private int n;                                // number of key-value pairs
-    private int m;                                // hash table size
-    private SequentialSearchST<Key, Value>[] st;  // array of linked-list symbol tables
+    /**
+     * number of key-value pairs
+     */
+    private int n;
+    /**
+     * hash table size
+     */
+    private int m;
+    /**
+     * / array of linked-list symbol tables
+     */
+    private SequentialSearchST<Key, Value>[] st;  
 
 
     /**
@@ -24,9 +35,11 @@ public class SeparateChainingHashST<Key, Value> {
         for (int i = 0; i < m; i++)
             st[i] = new SequentialSearchST<Key, Value>();
     } 
-
-    // resize the hash table to have the given number of chains,
-    // rehashing all of the keys
+    /**
+     * resises the hash table
+     * Time complexity is N as the loop iterates till the size of table
+     * @param      chains  The chains
+     */
     private void resize(int chains) {
         SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
         for (int i = 0; i < m; i++) {
@@ -39,14 +52,20 @@ public class SeparateChainingHashST<Key, Value> {
         this.st = temp.st;
     }
 
-    // hash value between 0 and m-1
+    /**
+     * // hash value between 0 and m-1
+     * Time compleixtyn is constant as each statement is executed only once
+     * @param      key   The key
+     *
+     * @return     { is of int type}
+     */
     private int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % m;
     } 
 
     /**
      * Returns the number of key-value pairs in this symbol table.
-     *
+     * Time compleixtyn is constant as each statement is executed only once
      * @return the number of key-value pairs in this symbol table
      */
     public int size() {
@@ -55,35 +74,31 @@ public class SeparateChainingHashST<Key, Value> {
 
     /**
      * Returns true if this symbol table is empty.
-     *
+     * Time compleixtyn is constant as each statement is executed only once
      * @return {@code true} if this symbol table is empty;
      *         {@code false} otherwise
      */
     public boolean isEmpty() {
         return size() == 0;
     }
-
     /**
-     * Returns true if this symbol table contains the specified key.
+     * { checks if the given key is in hash table or not}
+     *Time compleixtyn is constant as each statement is executed only once
+     * @param      key   The key
      *
-     * @param  key the key
-     * @return {@code true} if this symbol table contains {@code key};
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * @return     { return is of boolen type }
      */
     public boolean contains(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
     } 
-
-    /**
-     * Returns the value associated with the specified key in this symbol table.
-     *
-     * @param  key the key
-     * @return the value associated with {@code key} in the symbol table;
-     *         {@code null} if no such value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
-     */
+   /**
+    * Returns the value associated with the specified key in this symbol table
+    * TIme complexity is constant as it returns key directly
+    * @param      key   The key
+    *
+    * @return     { description_of_the_return_value }
+    */
     public Value get(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to get() is null");
         int i = hash(key);
@@ -91,14 +106,10 @@ public class SeparateChainingHashST<Key, Value> {
     } 
 
     /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
-     * if the specified value is {@code null}.
-     *
-     * @param  key the key
-     * @param  val the value
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * puts the given key value pair in hash table
+     * Time complexity is constant 
+     * @param      key   The key
+     * @param      val   The value
      */
     public void put(Key key, Value val) {
         if (key == null) throw new IllegalArgumentException("first argument to put() is null");
@@ -114,13 +125,10 @@ public class SeparateChainingHashST<Key, Value> {
         if (!st[i].contains(key)) n++;
         st[i].put(key, val);
     } 
-
     /**
-     * Removes the specified key and its associated value from this symbol table     
-     * (if the key is in this symbol table).    
-     *
-     * @param  key the key
-     * @throws IllegalArgumentException if {@code key} is {@code null}
+     * Removes the specified key and its associated value from this symbol table
+     * Time complexity is N as it need to search for the key to be delted
+     * @param      key   The key
      */
     public void delete(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to delete() is null");
--- practice/m22/22.1 Ransom Note/22.1 Ransom Note/SequentialSearchST.java
+++ practice/m22/22.1 Ransom Note/22.1 Ransom Note/SequentialSearchST.java
@@ -1,14 +1,36 @@
 
 public class SequentialSearchST<Key, Value> {
-    private int n;           // number of key-value pairs
-    private Node first;      // the linked list of key-value pairs
-
-    // a helper linked list data type
+    /**.
+     *number of key-value pairs
+     */
+    private int n; 
+    /**.
+     * // the linked list of key-value pairs
+     */
+    private Node first;      
+    /**.
+     * Class for node.
+     */
     private class Node {
+        /**.
+         * key of Key type
+         */
         private Key key;
+        /**
+         * val of Value type
+         */
         private Value val;
+        /**.
+         * next of Node type
+         */
         private Node next;
-
+        /**.
+         * Constructs the object.
+         *
+         * @param      key   The key
+         * @param      val   The value
+         * @param      next  The next
+         */
         public Node(Key key, Value val, Node next)  {
             this.key  = key;
             this.val  = val;
@@ -21,38 +43,38 @@ public class SequentialSearchST<Key, Value> {
      */
     public SequentialSearchST() {
     }
-
     /**
-     * Returns the number of key-value pairs in this symbol table.
-     * @return the number of key-value pairs in this symbol table
+     * returns the number of key value pairs
+     * Time complexity is constant as each statement is executed only once
+     * @return     { return of int type}
      */
     public int size() {
-        return n;
-    }
-
+        return n;}
     /**
-     * Is this symbol table empty?
-     * @return {@code true} if this symbol table is empty and {@code false} otherwise
+     * Determines if empty.
+     * TIme complexity is constant as each statement is executed only once
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return size() == 0;
     }
-
     /**
-     * Does this symbol table contain the given key?
-     * @param key the key
-     * @return {@code true} if this symbol table contains {@code key} and
-     *     {@code false} otherwise
+     * checks if the given key contains or not
+     * TIme complexity is constant as each statement is executed only once
+     * @param      key   The key
+     *
+     * @return     { True if contains else false }
      */
     public boolean contains(Key key) {
         return get(key) != null;
     }
 
     /**
-     * Returns the value associated with the given key.
-     * @param key the key
-     * @return the value associated with the given key if the key is in the symbol table
-     *     and {@code null} if the key is not in the symbol table
+     * Returns the value associated with the given key
+     * Time complexity is N as the loop iterates till it finds null
+     * @param      key   The key
+     *
+     * @return     { returns the value if key is present else rteruns null}
      */
     public Value get(Key key) {
         for (Node x = first; x != null; x = x.next) {
@@ -61,13 +83,13 @@ public class SequentialSearchST<Key, Value> {
         }
         return null;
     }
-
     /**
-     * Inserts the key-value pair into the symbol table, overwriting the old value
+     *  Inserts the key-value pair into the symbol table, overwriting the old value
      * with the new value if the key is already in the symbol table.
      * If the value is {@code null}, this effectively deletes the key from the symbol table.
-     * @param key the key
-     * @param val the value
+     * Time complexity is N as loop iterates till end
+     * @param      key   The key
+     * @param      val   The value
      */
     public void put(Key key, Value val) {
         if (val == null) {
@@ -88,14 +110,20 @@ public class SequentialSearchST<Key, Value> {
     /**
      * Removes the key and associated value from the symbol table
      * (if the key is in the symbol table).
+     * TIme complexity is N as it is call overloaded delete funciton.
      * @param key the key
      */
     public void delete(Key key) {
         first = delete(first, key);
     }
-
-    // delete key in linked list beginning at Node x
-    // warning: function call stack too large if table is large
+    /**
+     * delets the given key
+     * Time complexity is N as it is a recurisive function
+     * @param      x     { Node }
+     * @param      key   The key
+     *
+     * @return     { return is of Node type }
+     */
     private Node delete(Node x, Key key) {
         if (x == null) return null;
         if (key.equals(x.key)) {
