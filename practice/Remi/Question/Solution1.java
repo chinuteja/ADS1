790e548c9d510bb58e9d8efb0eec6f7064f5718f
chinuteja <chinuteja2008@msitprogram.net>
2019-02-25 09:48:51 +0530

    "ADS-1 remedial-4: 7/7 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- practice/Remi/Question/Solution1.java
+++ practice/Remi/Question/Solution1.java
@@ -28,12 +28,7 @@ class Solution1{
 
 		TreeMap<Double, Integer> sfreq = new TreeMap<>();
 
-		
-
-		// HashMap<Double, Integer> freq = new HashMap<Double, Integer>();
 		HashMap<Integer, Double> hm = new HashMap<Integer, Double>();
-
-		
 
 		Scanner ip = new Scanner(System.in);
 		int ips = Integer.parseInt(ip.nextLine());
@@ -45,7 +40,6 @@ class Solution1{
 			v = Double.parseDouble(str[1]);
 			hm.put(k,v);
 		}
-		// System.out.println(hm);
 		
 		for (Map.Entry<Integer, Double> en : hm.entrySet()) {
 			Double ke = en.getValue();
@@ -58,9 +52,6 @@ class Solution1{
 				sfreq.put(ke,1);
 			}
 		}
-		// System.out.println("freq = "+freq);
-		// sfreq.putAll(freq);
-		// System.out.println("sfreq = "+sfreq);
 
 		int sum = 0;
 		for (Map.Entry<Double, Integer> en : sfreq.entrySet()) {
@@ -68,36 +59,25 @@ class Solution1{
 			sum+=en.getValue();
 			sfreq.put(en.getKey(),sum);
 		}
-		// System.out.println("Usfreq = "+sfreq);
+
 		Map<Integer, Double> hm1 = Solution1.sortByValue(hm);
 
 		for (Map.Entry<Integer, Double> en : hm1.entrySet()) { 
 			double i = en.getValue();
-			// System.out.println(i);
 			double id = sfreq.get(i)*100;
 			double per = (double)id/ips;
 			per = 100*per;
 			per = Math.round(per);
 			id = id/100;
-			id++;
+			// id++;
 			result.put(en.getKey(),per);
-			// System.out.println(" - - - "+result);
-
         }
-        // System.out.println("res = "+result);
+
         int res = Integer.parseInt(ip.nextLine());
         for (int i = 0;i< res ;i++) {
         	int data = Integer.parseInt(ip.nextLine());
         	System.out.println((double)(result.get(data)/100));     	
         }
-
-        // System.out.println(hm);
-        // System.out.println(hm1);
-        // System.out.println(freq);
-        // System.out.println(sfreq);
-        // System.out.println(result);
-
-
 
 	}
 
